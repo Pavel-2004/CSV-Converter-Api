720fae41fb7af8c9ec6868bd65d3a0b9b0eae630
Pavel Halko <65878619+Pavel-2004@users.noreply.github.com>
2022-04-13 00:51:33 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,21 @@
 //set the input which will take in the csv
 const input = document.getElementById("csv")
+const selectedTypeInput = document.getElementById("type")
 var name = ''
-
+var options = ["questrade", "TD", "RBC"]
+for (let i = 0; i < options.length; i++) {
+    if(i == 0){
+        document.getElementById("type").value = options[0]
+    }
+    option = document.createElement("option")
+    option.innerText = options[i]
+    option.value = options[i]
+    document.getElementById("type").append(option)
+}
 
 input.addEventListener('change', function(e){
     //this is temporary until more types are addeda
-    selectedType = 'questrade'
+    selectedType = selectedTypeInput.value
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -15,7 +25,7 @@ input.addEventListener('change', function(e){
     document.write(today);
 
     const reader = new FileReader()
-    name =  today + "_" + input.files[0]["name"] + "_TradersEdge.csv"
+    name =  today + "_traders_edge_"+ input.files[0]["name"]
     reader.onload = function (){
         content = initAll(reader.result, selectedType)
         result = errorCase(content)
