043157dd4b34c6ee52f01af02cea9eeb2723d91c
Pavel Halko <65878619+Pavel-2004@users.noreply.github.com>
2022-04-14 14:17:54 -0400

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,24 @@
 //change this in the case that other columns will be added into the new format
-var header = ["Account Num",	'Trade date',	'Settlement date',	'Symbol',	'Exchange',	'Security name',	'TE type',	'Broker type',	'num units',	'$price/unit',	'Amount']
+var header = ["Account #",	'Trade date',	'Settlement date',	'Symbol',	'Exchange',	'Security name',	'TE type',	'Broker type',	'#units',	'$price/unit',	'Amount']
 
+
+var filename
+
+function setFileName(name){
+
+    filename = name
+}
 
 function checkArrayEqual(arrayOne, arrayTwo){
-    console.log(arrayOne.length)
+
     if(arrayOne.length == arrayTwo.length){
         for (let i = 0; i < arrayOne.length; i++) {
             if(arrayOne[i] != arrayTwo[i]){
-                console.log(arrayOne[i], arrayTwo[i])
                 return false
             }
             return true
         }
     } else{
-        console.log("length")
         return false
     }
 
@@ -43,8 +48,6 @@ function mapToProperFormat(currentData, args){
     //maps everything out
     for (let i = 0; i < currentData.length; i++) {
         for (let j = 0; j < currentData[i].length; j++) {
-            //bug possible if JSON.parse could turn ints into strings
-
             if(Object.keys(args).includes(String(j))){
                 final[i][args[j]] = currentData[i][j]
             }
@@ -57,28 +60,41 @@ function mapToProperFormat(currentData, args){
     for (let i = 0; i < final.length; i++) {
         finalValue.push(final[i])
     }
-    console.log(finalValue)
-    return downloadCSV(finalValue, 'test.csv', 'text/csv;encoding:utf-8')
+
+    return arrayToCsv(finalValue, 'test.csv', 'text/csv;encoding:utf-8')
 
 }
 
-
-
-
-//download the csv
-function downloadCSV(data, fileName, mimeType){
-    csvContent = 'data:text/csv;charset=utf-8,'
-    data.forEach(function(rowArray) {
-        let row = rowArray.join(",");
-        csvContent += row + "\r\n";
-    });
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    console.log(name)
-    link.setAttribute("download", name);
-    document.body.appendChild(link); // Required for FF
-
-    link.click();
-    return true
+function arrayToCsv(data, type){
+    final = data.map(row =>
+        row
+            .map(String)
+            .map(v => v.replaceAll('"', '""'))
+            .map(v => `"${v}"`)
+            .join(',')
+    ).join('\r\n');
+    downloadBlob(final, type)
 }
+
+
+function downloadBlob(content, contentType) {
+    today = new Date();
+    dd = String(today.getDate()).padStart(2, '0');
+    mm = String(today.getMonth() + 1).padStart(2, '0');
+    yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    filename = today + "_traders_edge_"+ input.files[0]["name"]
+
+
+
+    console.log(filename)
+    var blob = new Blob([content], { type: contentType });
+    var url = URL.createObjectURL(blob);
+
+    var pom = document.createElement('a');
+    pom.href = url;
+    pom.setAttribute('download', filename);
+    pom.click();
+}
+
+
